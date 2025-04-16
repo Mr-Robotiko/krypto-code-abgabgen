9b6bb845f5f53182329b981c77e4937e39ff623e
Raphael Tack <raphael.tack@partner.ibm.com>
2025-04-16 10:37:25 +0200

    add test case for task

--- math_algortithms/iterated_squared/test_main.py
+++ math_algortithms/iterated_squared/test_main.py
@@ -1,14 +1,17 @@
 import unittest
-from main import iterated_squares
+from main import square_and_multiply
 
 
 class TestIteratedSquaresAlgorithm(unittest.TestCase):
 
     def test_5_pow_11_mod_13(self):
-        self.assertEqual(iterated_squares(5,11,13), 8)
+        self.assertEqual(square_and_multiply(5,11,13), 8)
 
     def test_17_pow_19_mod_23(self):
-        self.assertEqual(iterated_squares(17,19,23), 5)
+        self.assertEqual(square_and_multiply(17,19,23), 5)
+
+    def test_888_pow_333_mod_11111(self):
+        self.assertEqual(square_and_multiply(8888, 333, 11111), 6223)
 
 
 if __name__ == '__main__':
