ae9040848eb99a1e8e5c692e274b741dda2ead87
Raphael Tack <raphael.tack@partner.ibm.com>
2025-04-16 10:42:32 +0200

    delete space

--- math_algortithms/iterated_squared/main.py
+++ math_algortithms/iterated_squared/main.py
@@ -1,26 +1,24 @@
-def square_and_multiply(base: int, exponent: int, mod: int) -> int:
-    if exponent == 0:
+def square_and_multiply(base: int, power: int, mod: int) -> int:
+    if power == 0:
         return 1
-
-    if exponent % 2 == 0:
-        half = square_and_multiply(base, exponent // 2, mod)
-        return (half * half) % mod
-
-    else:
-        return (base * square_and_multiply(base, exponent - 1, mod)) % mod
+    if power % 2 == 0: # b_n = 0
+        x: int = square_and_multiply(base, power // 2, mod)
+        return (x * x) % mod
+    else:   # b_n = 1
+        return (base * square_and_multiply(base, power - 1, mod)) % mod
 
 
 def main():
     check: bool = True
     while check:
-        basis: int = int(input("Was ist die Basis:\t"))
+        base: int = int(input("Was ist die Basis:\t"))
         power: int = int(input("Was ist die Potenz:\t"))
         mod: int = int(input("Was ist das Modulo:\t"))
 
-        print(f"Löse: {basis}^{power} = x mod {mod}\t")
-        x: int = square_and_multiply(basis, power, mod)
+        print(f"Löse: {base}^{power} = x mod {mod}\t")
+        x: int = square_and_multiply(base, power, mod)
 
-        print(f"{basis}^{power} = {x} mod {mod}")
+        print(f"{base}^{power} = {x} mod {mod}")
 
 
 if __name__ == '__main__':
