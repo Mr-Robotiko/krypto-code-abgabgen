d7e7f4dd79ef9cdbe22a5e7513d49bc3f1e5c23c
Raphael Tack <raphael.tack@partner.ibm.com>
2025-04-21 17:44:18 +0200

    refactor the square and muliply algorithm

--- math_algortithms/square_and_multiply/main.py
+++ math_algortithms/square_and_multiply/main.py
@@ -1,16 +1,24 @@
-def square_and_multiply(base: int, power: int, mod: int) -> int:
+def square_and_multiply(base: int, power: int, mod: int, depth=0) -> int:
+    indent = "  " * depth
+
     if power == 0:
         return 1
-    if power % 2 == 0: # b_n = 0
-        x: int = square_and_multiply(base, power // 2, mod)
-        return (x * x) % mod
-    else:   # b_n = 1
-        return (base * square_and_multiply(base, power - 1, mod)) % mod
+
+    if power % 2 == 0:
+        print(f"{indent}power = {power} (gerade) → rechne ({base}^{power // 2})² mod {mod}")
+        x = square_and_multiply(base, power // 2, mod, depth + 1)
+        result = (x * x) % mod
+        print(f"{indent}→ ({x}²) mod {mod} = {result}")
+        return result
+    else:
+        print(f"{indent}power = {power} (ungerade) → rechne {base} * ({base}^{power - 1}) mod {mod}")
+        result = (base * square_and_multiply(base, power - 1, mod, depth + 1)) % mod
+        print(f"{indent}→ {base} * a^{power - 1} mod {mod} = {result}")
+        return result
 
 
 def main():
-    check: bool = True
-    while check:
+    while True:
         base: int = int(input("Was ist die Basis:\t"))
         power: int = int(input("Was ist die Potenz:\t"))
         mod: int = int(input("Was ist das Modulo:\t"))
@@ -19,6 +27,7 @@ def main():
         x: int = square_and_multiply(base, power, mod)
 
         print(f"{base}^{power} = {x} mod {mod}")
+        print(f"{x = }")
 
 
 if __name__ == '__main__':
