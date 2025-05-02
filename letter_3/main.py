df090cc7ee0fa7cf33beab225e0d05c6190ec9d7
Raphael Tack <raphael.tack@partner.ibm.com>
2025-05-02 09:46:16 +0200

    add round keys and their tests

--- letter_3/main.py
+++ letter_3/main.py
@@ -1,16 +1,33 @@
+from typing import Dict, List
 
 
 class Schiffy128:
 
     def __init__(self):
         self.s0: int = 170
+        self.inital_key: int = 0xdeadbeef000000000000000badc0ffee
+        self.keys: List[int] = list()
+        self.lookup_table: List[int] = list()
 
-    def s_box(self, number: int = 0) -> int:
-        for x in range(number + 1):
-            self.s0 = 170 if x == 0 else (37 * self.s0 + 9) % 256
+        # Fill lookuptable and the round keys
+        self.s_box()
+        self.ksa()
+
+    def s_box(self) -> None:
+        for x in range(256):
+            self.lookup_table.append(170 if x == 0 else (37 * self.lookup_table[x-1] + 9) % 256)
+
+    def rotate_left(self, i: int) -> int:
+        if i == 0:
+            return self.inital_key
+        else:
+            return ((self.keys[i-1] << (7 * i) % 128) | (self.keys[i-1] >> (128 - (7 * i) % 128))) & ((1 << 128) - 1)
+
+    def ksa(self) -> None:
+        for i in range(32):
+            self.keys.append(self.rotate_left(i) ^ 0xabcdef)
 
 
 if __name__ == '__main__':
     schiffy: Schiffy128 = Schiffy128()
-    s = schiffy.s_box(1)
-    print(schiffy.s0)
+    print(hex(schiffy.keys[31]))
--- letter_3/test_main.py
+++ letter_3/test_main.py
@@ -6,28 +6,42 @@ class TestExtensionAttack(unittest.TestCase):
 
     def test_s_0(self):
         schiffy: Schiffy128 = Schiffy128()
-        schiffy.s_box()
-        self.assertEqual(schiffy.s0, 170)
+        self.assertEqual(schiffy.lookup_table[0], 170)
 
     def test_s_1(self):
         schiffy: Schiffy128 = Schiffy128()
-        schiffy.s_box(1)
-        self.assertEqual(schiffy.s0, 155)
+        self.assertEqual(schiffy.lookup_table[1], 155)
 
     def test_s_2(self):
         schiffy: Schiffy128 = Schiffy128()
-        schiffy.s_box(2)
-        self.assertEqual(schiffy.s0, 112)
+        self.assertEqual(schiffy.lookup_table[2], 112)
 
     def test_s_123(self):
         schiffy: Schiffy128 = Schiffy128()
-        schiffy.s_box(123)
-        self.assertEqual(schiffy.s0, 33)
+        self.assertEqual(schiffy.lookup_table[123], 33)
 
     def test_s_255(self):
         schiffy: Schiffy128 = Schiffy128()
-        schiffy.s_box(255)
-        self.assertEqual(schiffy.s0, 205)
+        self.assertEqual(schiffy.lookup_table[255], 205)
+
+    def test_k_0(self):
+        schiffy: Schiffy128 = Schiffy128()
+        self.assertEqual(schiffy.keys[0], 0xdeadbeef000000000000000bad6b3201)
+
+    def test_k_1(self):
+        schiffy: Schiffy128 = Schiffy128()
+        self.assertEqual(schiffy.keys[1], 0x56df778000000000000005d6b532cd00)
+
+    def test_k_2(self):
+        schiffy: Schiffy128 = Schiffy128()
+        self.assertEqual(schiffy.keys[2], 0xdde00000000000000175ad4cb3ebd858)
+
+    def test_k_31(self):
+        schiffy: Schiffy128 = Schiffy128()
+        self.assertEqual(schiffy.keys[31], 0x770feb4b3180dc3bc09870bd38e2cb5f)
+
+    def test_zero_block(self):
+        schiffy: Schiffy128 = Schiffy128()
 
 
 if __name__ == '__main__':
