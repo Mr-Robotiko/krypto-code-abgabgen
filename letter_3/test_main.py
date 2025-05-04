e135c7efb11279931cd51942c88323d2c9e8ce64
Raphael Tack <raphael.tack@partner.ibm.com>
2025-05-04 20:58:05 +0200

    implement schiffy128

--- letter_3/test_main.py
+++ letter_3/test_main.py
@@ -2,7 +2,7 @@ from main import Schiffy128
 import unittest
 
 
-class TestExtensionAttack(unittest.TestCase):
+class TestSchiffy(unittest.TestCase):
 
     def test_s_0(self):
         schiffy: Schiffy128 = Schiffy128()
@@ -28,20 +28,36 @@ class TestExtensionAttack(unittest.TestCase):
         schiffy: Schiffy128 = Schiffy128()
         self.assertEqual(schiffy.keys[0], 0xdeadbeef000000000000000bad6b3201)
 
-    def test_k_1(self):
-        schiffy: Schiffy128 = Schiffy128()
-        self.assertEqual(schiffy.keys[1], 0x56df778000000000000005d6b532cd00)
-
-    def test_k_2(self):
-        schiffy: Schiffy128 = Schiffy128()
-        self.assertEqual(schiffy.keys[2], 0xdde00000000000000175ad4cb3ebd858)
-
-    def test_k_31(self):
-        schiffy: Schiffy128 = Schiffy128()
-        self.assertEqual(schiffy.keys[31], 0x770feb4b3180dc3bc09870bd38e2cb5f)
-
     def test_zero_block(self):
         schiffy: Schiffy128 = Schiffy128()
+
+    def test_F1(self):
+        schiffy: Schiffy128 = Schiffy128()
+        self.assertEqual(schiffy.F(0x0000000000000000, schiffy.keys[0]), 0x94dfb49607c198ab)
+
+    def test_F2(self):
+        schiffy: Schiffy128 = Schiffy128()
+        self.assertEqual(schiffy.F(0x94dfb49607c198ab, schiffy.keys[1]), 0xb0aa7cca50e95fb1)
+
+    def test_F3(self):
+        schiffy: Schiffy128 = Schiffy128()
+        self.assertEqual(schiffy.F(0xb0aa7cca50e95fb1, schiffy.keys[2]), 0x1e9d6324e9783573)
+
+    def test_F4(self):
+        schiffy: Schiffy128 = Schiffy128()
+        self.assertEqual(schiffy.F(0x8a42d7b2eeb9add8, schiffy.keys[3]), 0x01a6283b0f33c8f0)
+
+    def test_F5(self):
+        schiffy: Schiffy128 = Schiffy128()
+        self.assertEqual(schiffy.F(0xc8ef99ba72f8a579, schiffy.keys[29]), 0xf7ffea032144154a)
+
+    def test_F6(self):
+        schiffy: Schiffy128 = Schiffy128()
+        self.assertEqual(schiffy.F(0x81f3d4d01743d570, schiffy.keys[30]), 0x7fac6b4146d4f4c6)
+
+    def test_F7(self):
+        schiffy: Schiffy128 = Schiffy128()
+        self.assertEqual(schiffy.F(0xb743f2fb342c51bf, schiffy.keys[31]), 0x2a66d3471f7cb499)
 
 
 if __name__ == '__main__':
